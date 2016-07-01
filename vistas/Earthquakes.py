943ea74e85447c56425da4e074c3e2339452ff02
Churritzu <churritzu@yahoo.com>
2016-06-30 18:12:10 -0700

    Se agrego el paquete vistas

---
+++ vistas/Earthquakes.py
@@ -0,0 +1,8 @@
+import Globales
+
+class EarthquakesView:
+	def __init__(self):
+		print(Globales.title())
+
+if __name__ == "__main__":
+	pass
---
+++ vistas/Help.py
@@ -0,0 +1,19 @@
+import sys
+import Globales
+
+class HelpView:
+	def __init__(self):
+		print(Globales.title())
+		print("Usage: earthFeed [Options(s)]\n")
+		print("\t-h, --help\tGet this help list")
+		print("")
+		sys.exit()
+
+	@staticmethod
+	def need_help(opts):
+		for opt, val in opts:
+			if opt == "-h" or opt == "--help":
+				return True
+
+if __name__ == "__main__":
+	pass
