2c9d1b1afa2d5520805688cb5aa8127bbad95489
Churritzu <churritzu@yahoo.com>
2016-07-02 23:21:45 -0700

    Actualizo HelpView para incluir search, magnitud, wait

--- vistas/Help.py
+++ vistas/Help.py
@@ -5,7 +5,10 @@ class HelpView:
 	def __init__(self):
 		print(Globales.title())
 		print("Usage: earthFeed [Options(s)]\n")
-		print("\t-h, --help\tGet this help list")
+		print("\t-h, --help\t\tGet this help list")
+		print("\t--search=(all|local)\tThe type of search can be all or local (default=all)")
+		print("\t--magnitude=(2.0)\tThe min magnitude of the earthquake (default=2.0)")
+		print("\t--wait=(5)\t\tIs the time it takes to update the data in minutes (default=5)")
 		print("")
 		sys.exit()
 
