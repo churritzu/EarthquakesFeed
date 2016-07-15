5c5422759a27f6a32284d1443ca644b69a5237d4
Churritzu <churritzu@yahoo.com>
2016-07-15 16:13:28 -0700

    Se agrego el frame principal a la ventana

--- vistas/MainWindow.py
+++ vistas/MainWindow.py
@@ -3,16 +3,30 @@ from tkinter import ttk
 from vistas.HelpMenu import HelpMenu
 from vistas.FileMenu import FileMenu
 
-#TODO: Solo dejar codigo relacionado a la ventana principal aqui.
-#TODO: Tengo que refactorizar todas las ventanas en sus propias clases
-
 class MainWindow:
+	init_width =800
+	init_height = 600
+
 	def __init__(self):
 		self.main_window = Tk()
 		self.main_window.title("Earthquakes Feeds")
-		self.main_window.config(height=600, width=800, background="#222")
-
+		self.main_window.config(height=self.init_height, width=self.init_width)
 		self.main_window.config(menu=self.top_menu())
+
+		#Styles
+		sf = ttk.Style()
+		sf.configure("TFrame", background="#333")
+		sf.configure("TLabel", background="#333", foreground="#fff")
+
+		#Frames
+		main_frame = ttk.Frame(self.main_window, padding=5, width=self.init_width, height=self.init_height)
+		main_frame.pack_propagate(0)
+
+		#Label
+		ttk.Label(main_frame).pack()
+
+		main_frame.pack(fill=BOTH, expand=1)
+
 		self.main_window.mainloop()
 
 	def top_menu(self):
@@ -22,5 +36,8 @@ class MainWindow:
 		main_menu.add_cascade(label="File", menu=file.get_menu())
 		main_menu.add_cascade(label="Help", menu=help.get_menu())
 		return main_menu
+
+	def search(self):
+		print("Searching....")
 
 if __name__ == "__main__": pass
