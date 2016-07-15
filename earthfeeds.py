a8f6fd19fb1f38e581274da4570a3faee6382f72
Churritzu <churritzu@yahoo.com>
2016-07-14 21:17:22 -0700

    Se elimino todo lo inesesario de earthfeeds.
    Solo se dejo la llamada a la funcion principal
    la cual solo crea una ventana principal

--- earthfeeds.py
+++ earthfeeds.py
@@ -1,27 +1,7 @@
 #!/usr/bin/env python3
-import getopt
-import sys
-from misc import Globales
-from vistas.Earthquakes import EarthquakesView
-from vistas.Help import HelpView
 from vistas.MainWindow import MainWindow
 
-class Main:
-	def __init__(self, argv):
-		if argv:
-			try:
-				optsStr = "h"
-				opstList = ["help", "search=", "magnitude=", "wait=", "latitud=", "longitud=", "north=", "south=", "east=", "west="]
-				opts, args = getopt.getopt(argv, optsStr, opstList)
-				self.put_view(opts)
-			except getopt.GetoptError: self.put_view([("--help","")])
-		else: self.put_view()
-
-	def put_view(self, opts=None):
-		if HelpView.need_help(opts): HelpView()
-		else: EarthquakesView(opts)
-
-if __name__ == "__main__":
+def main():
 	app = MainWindow()
-	#Globales.clean_screen()
-	#Main(sys.argv[1:])
+
+if __name__ == "__main__": main()
